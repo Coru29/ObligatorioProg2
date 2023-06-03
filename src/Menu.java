1c8b33fcb175087e1274c004ccc24908ea491032
coru <acoru2002@gmail.com>
2023-06-03 17:01:32 -0300

    se creo el menu

---
+++ src/Menu.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    private final Scanner scanner;
+
+    public Menu() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        while (true) {
+            System.out.println("Ingrese una opción:");
+            System.out.println("1. Listar los 10 pilotos más mencionados");
+            System.out.println("2. Top 15 usuarios con más tweets");
+            System.out.println("3. Cantidad de hashtags distintos para un día dado");
+            System.out.println("4. Hashtag más usado para un día dado");
+            System.out.println("5. Top 7 cuentas con más favoritos");
+            System.out.println("6. Cantidad de tweets con una palabra o frase específica");
+            System.out.println("7. Salir");
+
+            int opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ingrese el mes y año (en formato MM-YYYY):");
+                    String mesAno = scanner.next();
+                    // la función para listar los pilotos más mencionados
+                    break;
+                case 2:
+                    // la función para mostrar el top de usuarios con más tweets
+                    break;
+                case 3:
+                    System.out.println("Ingrese la fecha (en formato YYYY-MM-DD):");
+                    String fecha = scanner.next();
+                    // la función para mostrar la cantidad de hashtags distintos
+                    break;
+                case 4:
+                    System.out.println("Ingrese la fecha (en formato YYYY-MM-DD):");
+                    String fechaHash = scanner.next();
+                    // la función para mostrar el hashtag más usado
+                    break;
+                case 5:
+                    // la función para mostrar las top cuentas con más favoritos
+                    break;
+                case 6:
+                    System.out.println("Ingrese la palabra o frase a buscar:");
+                    String busqueda = scanner.next();
+                    // la función para contar tweets con una frase específica
+                    break;
+                case 7:
+                    System.out.println("Hasta luego!");
+                    return;
+                default:
+                    System.out.println("Opción no reconocida");
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Menu elMenu = new Menu();
+
+        elMenu.start();
+    }
+
+
+}
+
+
